66c3ce15ceaaae7174a07f2d45b19a155a0c9a8b
SMT061 <SMT061@118.40.119.225>
2020-09-16 17:15:17 +0900

    baby 멤버 vo,dao

--- src/baby/model/BabyMemberVO.java
+++ src/baby/model/BabyMemberVO.java
@@ -7,59 +7,82 @@ public class BabyMemberVO {
 	private String pw;
 	private String name;
 	private int age;
-	private int number;
+	private int hp;
 	private int baby_age;
 	
-
-
-	public BabyMemberVO(String id, String pw, String name, int age, int number, int baby_age) {
-		
+	
+	public BabyMemberVO(String id, String pw, String name, int age, int hp, int baby_age) {
+		super();
 		this.id = id;
 		this.pw = pw;
 		this.name = name;
 		this.age = age;
-		this.number = number;
+		this.hp = hp;
 		this.baby_age = baby_age;
 	}
-	
-	
-	
+
+
 	public String getId() {
 		return id;
 	}
+
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
+
 	public String getPw() {
 		return pw;
 	}
+
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
 	public int getAge() {
 		return age;
 	}
+
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public int getNumber() {
-		return number;
+
+
+	public int getHp() {
+		return hp;
 	}
-	public void setNumber(int number) {
-		this.number = number;
+
+
+	public void setHp(int hp) {
+		this.hp = hp;
 	}
+
+
 	public int getBaby_age() {
 		return baby_age;
 	}
+
+
 	public void setBaby_age(int baby_age) {
 		this.baby_age = baby_age;
 	}
+	
+
+
 	
 	
 	
