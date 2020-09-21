6eea27feedf76b4e9787c47e06f6c0b884968af4
SMT033 <SMT033@59.3.254.196>
2020-09-21 16:05:34 +0900

    다시했다잉

--- WebContent/assets/js/AhangCategory1.js
+++ WebContent/assets/js/AhangCategory1.js
@@ -1,2 +1,19 @@
 var slide = document.querySelector('.slides'),
-	slide1 = document.querySelecttorAll('.slides li')
+	slide1 = document.querySelecttorAll('.slides li'),
+	
+	currentIdx =0,
+	slideCount =slide.lenght,
+	prevBtn = document.querySelector('.prev'),
+	slidewidth =300,
+	slidemargin =30,
+	nextBtn = document.querySelector('.next');
+	
+	slide.style.width =(slidewidth+slidemargin)*slideCount-slidemargin +'px';
+	
+	function moveSlide(num){
+		slide.style.left = -num *330 +'px';
+		currentIdx =num;
+	}
+	nextBtn.addEventListener('click', function(){
+		moveSlide
+	});
