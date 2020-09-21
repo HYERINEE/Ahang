d1c30fd6002f3e73479af622131031633da46f77
SMT015 <SMT015@210.105.224.109>
2020-09-21 11:40:06 +0900

    vo 수정

--- src/baby/model/NoticeVO.java
+++ src/baby/model/NoticeVO.java
@@ -3,22 +3,25 @@ package baby.model;
 public class NoticeVO {
 	
 	private int notice_num;
-	private String ID;
-	private String Review;
+	private String id;
+	private String review;
 	private int star;
-	private String model_id;
 	private String brand_id;
+	private String model_id;
 	
 	
-	public NoticeVO(int notice_num, String iD, String review, int star, String model_id, String brand_id) {
+	
+	public NoticeVO(int notice_num, String id, String review, int star,String brand_id, String model_id) {
 		super();
 		this.notice_num = notice_num;
-		ID = iD;
-		Review = review;
+		this.id = id;
+		this.review = review;
 		this.star = star;
-		this.model_id = model_id;
 		this.brand_id = brand_id;
+		this.model_id = model_id;
+		
 	}
+
 
 
 	public int getNotice_num() {
@@ -26,29 +29,35 @@ public class NoticeVO {
 	}
 
 
+
 	public void setNotice_num(int notice_num) {
 		this.notice_num = notice_num;
 	}
 
 
-	public String getID() {
-		return ID;
+
+	public String getId() {
+		return id;
 	}
 
 
-	public void setID(String iD) {
-		ID = iD;
+
+	public void setId(String id) {
+		this.id = id;
 	}
+
 
 
 	public String getReview() {
-		return Review;
+		return review;
 	}
+
 
 
 	public void setReview(String review) {
-		Review = review;
+		this.review = review;
 	}
+
 
 
 	public int getStar() {
@@ -56,19 +65,11 @@ public class NoticeVO {
 	}
 
 
+
 	public void setStar(int star) {
 		this.star = star;
 	}
 
-
-	public String getModel_id() {
-		return model_id;
-	}
-
-
-	public void setModel_id(String model_id) {
-		this.model_id = model_id;
-	}
 
 
 	public String getBrand_id() {
@@ -76,19 +77,23 @@ public class NoticeVO {
 	}
 
 
+
 	public void setBrand_id(String brand_id) {
 		this.brand_id = brand_id;
 	}
 
 
-	
-	
-	@Override
-	public String toString() {
-		return "NoticeVO [notice_num=" + notice_num + ", ID=" + ID + ", Review=" + Review + ", star=" + star
-				+ ", model_id=" + model_id + ", brand_id=" + brand_id + "]";
+
+	public String getModel_id() {
+		return model_id;
 	}
-	
+
+
+
+	public void setModel_id(String model_id) {
+		this.model_id = model_id;
+	}
+
 	
 	
 	
